36e087418c02f00d0f1c700af4944a9b7c9b15fc
makrozai <marco.condoric@gmail.com>
2019-10-10 15:08:52 -0500

    chore: implement header with uikit

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <header className="uk-margin" uk-margin="true">
+        <h1 className="uk-text-center">Eventos</h1>
+      </header>
+     );
+  }
+}
+ 
+export default Header;
